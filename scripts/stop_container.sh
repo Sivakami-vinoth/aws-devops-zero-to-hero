5b9794ac27f75fcf0944edd353d7e12da05231a9
Hi, I’m @Sivakami-vinoth <125202974+Sivakami-vinoth@users.noreply.github.com>
2025-06-04 21:38:29 +0400

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Stop the running container (if any)
-containerid = docker ps| awk -f " " '{print$1}
-docker rm -f $containerid
+#containerid = docker ps| awk -f " " '{print$1}
+#docker rm -f $containerid
